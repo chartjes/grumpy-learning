a28b304ed65a2821353e4ee8d234a1f510cfa06c
Ubuntu <ubuntu@ip-172-26-34-224.ec2.internal>
2017-01-05 01:41:08 +0000

    New changes for the site

--- index.php
+++ index.php
@@ -22,119 +22,36 @@
 <!-- Header and Nav -->
 <div class="row">
     <div class="twelve columns">
-        <h1 align="center">Let a grumpy programmer teach you how to write better, more maintainable code</h1>
+        <h1 align="center">Let a grumpy programmer teach you how to build a foundation for testing your PHP code</h1>
     </div>
 </div>
 
 <!-- End Header and Nav -->
 
-
-<!-- First Band (Slider) -->
-<!-- The Orbit slider is initialized at the bottom of the page by calling .orbit() on #slider -->
-<div class="row">
-    <ul class="example-orbit" data-orbit>
-        <li><img src="https://grumpy-learning.com/images/slide-01.jpg" alt="Testable code is good. Testable apps are better."/></li>
-        <li><img src="https://grumpy-learning.com/images/slide-02.jpg" alt="Intimidating legacy code can be refactored to have automated tests." /></li>
-        <li><img src="https://grumpy-learning.com/images/slide-03.jpg" alt="Learn why Dependency Injection is a secret weapon."/></li>
-        <li><img src="https://grumpy-learning.com/images/slide-04.jpg" alt="Databases? API calls? We can test that."/></li>
-        <li><img src="https://grumpy-learning.com/images/slide-05.jpg" alt="Small, well tested modules of code let you go home on time."/></li>
-        <li><img src="https://grumpy-learning.com/images/slide-06.jpg" alt="You don't have to be scared to push code live."/></li>
-    </ul>
-    <hr />
-</div>
-
-<div class="row">
-    <div class="columns">
-        <p>
-        <a href="https://www.e-junkie.com/ecom/gb.php?c=cart&cl=243577&ejc=2" target="ej_ejc" class="ec_ejc_thkbx"><img src="https://www.e-junkie.com/ej/ej_view_cart.gif" border="0" alt="View Cart"/></a>
-        </p>
-    </div>
-</div>
-<div class="row">
-    <div class="large-12 columns">
-        <h4>The Grumpy Programmer's Little Book of Hack</h4>
-	<img src="images/grumpy-hack.jpg">
-        <h6>Learn how Facebook's Hack can bring consistency to your legacy PHP code</h6>
-	<p><a href="https://leanpub.com/grumpy-hack">Click here to buy via LeanPub - $12</a></p>
-    </div>
-</div>
 <div class="row">
     <div class="large-6 columns">
-        <h4>The Grumpy Programmer's Guide To Building Testable Applications In PHP</h4>
-	<img src="images/grumpy-testing.jpg">
+        <h4>The Grumpy Programmer's Minimum Viable Tests</h4>
+	<img width=240 height=360 src="images/mvt-title-page-web.jpg">
+        <h6>What do you really need to know to get started testing your PHP code?</h6>
+	<p><a href="https://leanpub.com/minimumviabletests">Click here to buy via LeanPub - $29</a></p>
     </div>
     <div class="large-6 columns">
         <h4>The Grumpy Programmer's PHPUnit Cookbook</h4>
 	<img src="images/grumpy-phpunit.jpg">
+	    <h6>Learning how to
+		use PHPUnit
+		doesn't have to
+		suck</h6>
+	    <p><a href="https://leanpub.com/grumpy-phpunit">Click here to buy via LeanPub - $29</a></p>
     </div>
 </div>
 <div class="row">
-    <div class="large-6 columns">
-    <h6>You want to test your PHP code but you don't know how</h6>
-    <p><a href="https://leanpub.com/grumpy-testing">Click here to buy via LeanPub - $20</a></p>
-    </div>
-    <div class="large-6 columns">
-    <h6>Learning how to
-        use PHPUnit
-        doesn't have to
-        suck</h6>
-    <p><a href="https://leanpub.com/grumpy-phpunit">Click here to buy via LeanPub - $29</a></p>
-    </div>
-</div>
-<div class="row">
-    <div class="large-12" align="center">
-        <a href="https://leanpub.com/b/grumpy">Click here to buy them both as part of a bundle for just $40</a>
-    </div>
-</div>
-<div class="row">
-    <hr>
-</div>
-<div class="row">
-    <div class="large-6 columns">
-        <div class="row">
-            <h4>PHP Testing Bootcamp Sessions</h4>
-            <div class="flex-video">
-                <iframe width="320" height="240" src="//www.youtube.com/embed/GXi941I8jys" frameborder="0" allowfullscreen></iframe>
-            </div>
-            <div class="small-3 columns">
-                <p>
-                <b>$49.50</b> <a href="https://www.e-junkie.com/ecom/gb.php?c=cart&i=1219892&cl=243577&ejc=2" target="ej_ejc" class="ec_ejc_thkbx"><img src="https://www.e-junkie.com/ej/ej_add_to_cart.gif" border="0" alt="Add to Cart"/></a> 
-                </p>
-            </div>
-        </div>
-        <div class="row">
-            <h6>You're tired of trying to figure out testing PHP code in your own</h6>
-            <p>
-            You've been working on refactoring your code to be <a href="http://grumpy-testing.com">more testable</a> and
-            you've got some no-nonsense info on <a href="http://grumpy-phpunit.com">using PHPUnit</a>.
-            </p>
-            <p>
-            But how do you
-            <b>put it all together</b>?
-            </p>
-            <p><i>DRM-free 640x480 MPEG-4 with audio</i></p>
-            <p><a href="/testing-bundle.php">Interested? Click here to find out more</a></p>
-        </div>
-    </div>
-    <div class="large-6 columns">
-        <h4>Refactoring Using Tests</h4>
-        <div class="row">
-            <div class="flex-video">
-                <iframe width="320" height="240" src="//www.youtube.com/embed/KlZHSJKfbSg" frameborder="0" allowfullscreen></iframe>
-            </div>
-        </div>
-        <div class="row">
-            <h6>Watch code be refactored and tested in real-time</h6>
-            <p>
-            A <b>multi-video</b> series of Chris <b>refactoring</b> an 11-year-old PHP application
-            to be <b>more organized</b> and with <b>great test coverage</b>.
-            </p>
-            <p>This series is lightly-edited, <b>watch Chris work</b> in real-time!</p> 
-            <p><i>DRM-free 1280x720 MPEG-4 with audio</i></p>
-            <p><a href="/refactoring-using-tests.php">Interested? Click here to find out more</a></p>
-        </div>
-    </div>
-</div>
+	<div class="twelve columns">
+		<h4>Learn To Test Like A Grumpy Programmer</h4>
+		<h6>Online instruction from one of PHP's best testing evangelists</h6>
+		<p>On January 20th, 2017, Grumpy Learning is running a small online class for up to 12 students. <a href="https://ti.to/grumpy-learning-workshops/learn-to-test-like-a-grumpy-programmer-january-20-2017">To buy tickets, click here</a></p>
+	</div>
+<div>
 <div>
     <!-- Call to Action Panel -->
     <div class="row">
@@ -173,16 +90,6 @@
 <script src="/js/vendor/jquery.js"></script>
 <script src="/js/vendor/fastclick.js"></script>
 <script src="/js/foundation.min.js"></script>
-<script type="text/javascript">
-$(document).foundation({
-    orbit: {
-        animation: 'slide',
-        timer_speed: 10000,
-        navigation_arrows: false,
-        pause_on_hover: false,
-    }
-});
-</script>
 <script language="javascript" type="text/javascript">
 <!--
 function EJEJC_lc(th) { return false; }
@@ -194,9 +101,5 @@ function EJEJC_config(th) {
 // -->
 </script>
 <script src='https://www.e-junkie.com/ecom/box.js' type='text/javascript'></script>
-<!-- FOXYCART -->
-<script src="//cdn.foxycart.com/grumpy-learning/loader.js" async defer></script>
-<!-- /FOXYCART -->
-
 </body>
 </html>

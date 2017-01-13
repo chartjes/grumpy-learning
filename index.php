d188cb41bc88c6fa13cc15ff6245777fa239cfee
Chris Hartjes <chartjes@littlehart.net>
2017-01-13 14:41:13 -0500

    New look and feel for the site

--- index.php
+++ index.php
@@ -1,105 +1,143 @@
 <!DOCTYPE html>
-
-<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
-<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
-<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
-<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<html>
 <head>
-<meta charset="utf-8" />
-
-<!-- Set the viewport width to device width for mobile -->
-<meta name="viewport" content="width=device-width" />
-
-<title>Learning To Program The Grumpy Way</title>
-
-<!-- Included CSS Files (Compressed) -->
-<link rel="stylesheet" href="css/normalize.css">
-<link rel="stylesheet" href="css/foundation.css">
-<script src="/js/vendor/custom.modernizr.js"></script>
+<meta charset="UTF-8" />
+<title>Learn To Test Like A Grumpy Programmer</title>
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="./assets/css/app.css">
+<script type="text/javascript">
+    WebFontConfig = {
+      google: { families: [ 'Asap:400italic,700italic:latin', 'Source+Sans+Pro:400,300,600,200,300italic,400italic,700:latin' ] }
+    };
+    (function() {
+      var wf = document.createElement('script');
+      wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+        '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+      wf.type = 'text/javascript';
+      wf.async = 'true';
+      var s = document.getElementsByTagName('script')[0];
+      s.parentNode.insertBefore(wf, s);
+    })();
+  </script>
 </head>
 <body>
-<!-- Header and Nav -->
-<div class="row">
-    <div class="twelve columns">
-        <h1 align="center">Let a grumpy programmer teach you how to build a foundation for testing your PHP code</h1>
+<header class="header header--centered">
+    <div class="container container--normal">
+        <h1 class="header__title">Grumpy Learning</h1>
+        <p class="header__tagline">Learn to test like a grumpy programmer</p>
     </div>
-</div>
+</header>
 
-<!-- End Header and Nav -->
-
-<div class="row">
-    <div class="large-6 columns">
-        <h4>The Grumpy Programmer's Minimum Viable Tests</h4>
-	<img width=240 height=360 src="images/mvt-title-page-web.jpg">
-        <h6>What do you really need to know to get started testing your PHP code?</h6>
-	<p><a href="https://leanpub.com/minimumviabletests">Click here to buy via LeanPub - $29</a></p>
-    </div>
-    <div class="large-6 columns">
-        <h4>The Grumpy Programmer's PHPUnit Cookbook</h4>
-	<img src="images/grumpy-phpunit.jpg">
-	    <h6>Learning how to
-		use PHPUnit
-		doesn't have to
-		suck</h6>
-	    <p><a href="https://leanpub.com/grumpy-phpunit">Click here to buy via LeanPub - $29</a></p>
-    </div>
-</div>
-<div class="row">
-	<div class="twelve columns">
-		<h4>Learn To Test Like A Grumpy Programmer</h4>
-		<h6>Online instruction from one of PHP's best testing evangelists</h6>
-		<p>On January 20th, 2017, Grumpy Learning is running a small online class for up to 12 students. <a href="https://ti.to/grumpy-learning-workshops/learn-to-test-like-a-grumpy-programmer-january-20-2017">To buy tickets, click here</a></p>
-	</div>
-<div>
-<div>
-    <!-- Call to Action Panel -->
-    <div class="row">
-        <div class="twelve columns">
-            <div class="panel">
-                <div class="row">
-                    <div class="twelve columns">
-                        We have a mailing list that you can join that drops some grumpy knowledge in your inbox roughly once every two weeks. We hate spam as much as you do, so we pledge to never give your email address to any third parties.<br><br>
-                        <!-- Begin MailChimp Signup Form -->
-                        <div id="mc_embed_signup" class="span8">
-                            <form action="http://grumpy-phpunit.us6.list-manage2.com/subscribe/post?u=161d0ed420649f1879e4985f1&amp;id=1a4c405c76" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-                                <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
-                                <div class="clear"><input type="submit" value="Add me to your mailing list!" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-                        </form>                  </div>
-                        <!--End mc_embed_signup-->
-                    </div>
-                </div>
+<main role="main">
+    <blockquote class="quote">
+        <div class="container container--widened">
+            <p class="quote__text">Chris's books helped me to get over that horrible early bump in the road that you find when you're learning to test. He helped everything click and made me understand not just how to test, but why you should do it. I liked them so much I've bought copies for other people! Thank you, Chris.</p>
+            <cite class="quote__byline"><a href="https://twitter.com/GeeH">Gary Hockin, Developer Relations at JetBrains</a></cite>
+        </div>
+    </blockquote>
+    <div class="container container--normal">
+        <div class="package" id="mvt-basic">
+            <a href="https://leanpub.com/minimumviabletests"><img src="images/mvt-title-page-web.jpg" class="u-image-pull-right" alt="Cover for Minimum Viable Tests"/></a>
+            <h2 class="section-heading" id="#mvt">Minimum Viable Tests</h2>
+            <p>You've been doing PHP development for a while but you find yourself constantly battling bugs, finding it hard to integrate bits of code together, and staying late to do deployments. You know there is a better way through test-driven development but you have no idea where to start.</p>
+            <p>You imagine yourself sitting down with an experienced developer -- just you and them, steadily working through problems and learning how to use test-driven development to design the API's and interfaces for your code while making sure nothing leaves development until you're 100% sure it's working.</p>
+            <p>In "Minimum Viable Tests" long-time PHP testing evangelist Chris Hartjes goes over what his experiences lead him to believe are the basic concepts surrounding test-driven development. You'll learn about:
+                <ul class="checkbox-list">
+                    <li class="checkbox-list__item">Chris' concept of "metatesting"</li>
+                    <li class="checkbox-list__item">The basics of what PHPUnit is and how it works</li>
+                    <li class="checkbox-list__item">The role of tests in the development process</li>
+                    <li class="checkbox-list__item">Effective use of test doubles</li>
+                    <li class="checkbox-list__item">Understanding how to mimic your application's environment via bootstrapping</li>
+                    <li class="checkbox-list__item">Writing wrappers around web API calls to make testing easier</li>
+                    <li class="checkbox-list__item">How fixtures and data providers help simplify your tests both today and tomorrow</li>
+                    <li class="checkbox-list__item">When you should use (and not abuse) monkey-patching tools</li>
+                    <li class="checkbox-list__item">The Arrange-Act-Assert test-writing strategy</li>
+                    <li class="checkbox-list__item">How to refactor existing tests</li>
+                </ul>
+            </p>
+            <p>Chris has been watching and listening to people as they start their journey towards become test-driven developers. "Minimum Viable Tests" is designed to be your guide on the path to having well-tested, confidently-deployed PHP applications.</p>
+            <a href="https://leanpub.com/minimumviabletests" class="button package__button">
+                <span class="package__button-price">$29</span>
+                <span class="package__button-text">Buy Now Via LeanPub</span>
+            </a>
+        </div>
+        <div class="package" id="phpunit-cookbook-basic">
+            <a href="https://leanpub.com/grumpy-phpunit"><img src="images/grumpy-phpunit-title.png" class="u-image-pull-right" alt="Cover for Minimum Viable Tests"/></a>
+            <h2 class="section-heading" id="#phpunitcookbook">The Grumpy Programmer's PHPUnit Cookbook</h2>
+            <h3>Learning how to use PHPUnit doesn't have to suck</h3>
+            <p>PHPUnit is the most commonly-used unit testing framework for PHP developers.
+                While it's very powerful it also has a big learning curve. You know now is
+                the <b>time to master it</b> but you have no where to start. There's lots
+                of examples but you <b>need some guidance</b> on how to use it's features.
+            </p>
+            <p>
+                I'm a long-time PHP developer who has been promoting testing practices for almost a decade. For almost a decade I have spoken at conferences about best practices for writing tests and been recognized as an expert on unit testing. Now, I want to help you learn to use PHPUnit to create tests that give you confidence that your code is on the right track.
+            </p>
+            <p>You'll know exactly how to tackle your existing code. You'll learn how to drive new features with your tests. You'll write tests that run fast and keep you moving quickly. Here's what I cover:
+                <ul class="checkbox-list">
+                    <li class="checkbox-list__item">How PHPUnit actually works</li>
+                    <li class="checkbox-list__item">Creating test doubles</li>
+                    <li class="checkbox-list__item">Creating data providers</li>
+                    <li class="checkbox-list__item">Data fixtures that grow along with your application</li>
+                    <li class="checkbox-list__item">How PHPUnit can be used to test API's</li>
+                    <li class="checkbox-list__item">Using DBUnit</li>
+                    <li class="checkbox-list__item">How to test exceptions</li>
+                </ul>
+            </p>
+            <p>
+                My new ebook "The Grumpy Programmer's PHPUnit Cookbook" is a collection of tips and tricks for making PHPUnit do what you need it do in order to write tests for your code. With the information contained in this book you will get a jump start to make writing tests something you do effortlessly.
+            </p>
+            <a href="https://leanpub.com/grumpy-phpunit" class="button package__button">
+                <span class="package__button-price">$29</span>
+                <span class="package__button-text">Buy Now Via LeanPub</span>
+            </a>
+        </div>
+        <div class="callout-box callout-box--spaced">
+            <div class="callout-box__content-center">
+                <p>We have a mailing list that you can join that drops some grumpy knowledge in your inbox roughly once every two weeks. We hate spam as much as you do, so we pledge to never give your email address to any third parties.<br><br>
+                    <form action="http://grumpy-phpunit.us6.list-manage2.com/subscribe/post?u=161d0ed420649f1879e4985f1&amp;id=1a4c405c76" method="post" class="form-vertical" target="_blank">
+                        <div class="form-vertical__row">
+                            <label for="name" class="form-vertical__label">Email</label>
+                            <input type="text" value="" name="EMAIL" class="form-input-text" id="mce-EMAIL">
+                        </div>
+                        <div class="form-vertical__row--submit">
+                            <button type="submit" class="button">Add me to the Grumpy Learning Mailing List</button>
+                        </div>
+                    </form>
+                </p>
             </div>
         </div>
     </div>
-    <!-- Footer -->
-    <footer class="row">
-        <div class="twelve columns">
-            <hr />
-            <div class="row">
-                <div class="six columns">
-                    Got questions? <a href="mailto:chartjes@grumpy-learning.com">Email me</a> or find me on <a href="https://twitter.com/grmpyprogrammer">Twitter</a> as @grmpyprogrammer
-                </div>
-                <div class="six columns">
-                    <p>&copy; <?= date('Y') ?> Grumpy Learning Inc. All rights reserved</p>
-                </div>
+    <div class="author__container">
+        <div class="container container--grid">
+            <div class="author__left">
+                <img src="images/author-akrabat.jpg" alt="Chris Hartjes" class="author__small"/>
             </div>
+            <div class="author__right">
+              <p>Hi, I’m Chris Hartjes.</p>
+              <p>
+                  I've been building web applications using all kinds of tools (mostly PHP) and all kinds of techniques since 1998
+              </p>
+              <p>
+                  In 2003 I was part of a application launch so horrible I became determined to find a way to avoid that. Along the way
+                  I discovered testing techniques and haven't shut up about them since.
+              </p>
+              <p>I live in Milton, Ontario, Canada along with my long-suffering wife and two very patient daughters.</p>
+          </div>
+      </div>
+  </main>
+  <footer class="footer">
+      <div class="container container--grid">
+        <div class="footer__col footer__col--normal">
+            <p>Got questions? <a href="mailto:chartjes@grumpy-learning.com">Email me</a> or find me on <a href="https://twitter.com/grmpyprogrammer">Twitter</a> as @grmpyprogrammer</p>
         </div>
+        <div class="footer__bottom">
+          <p>&copy; <?= date('Y') ?> Grumpy Learning Inc. All Right Reserved</p>
+        </div>
+      </div>
     </footer>
-</div>
-<script src="/js/vendor/jquery.js"></script>
-<script src="/js/vendor/fastclick.js"></script>
-<script src="/js/foundation.min.js"></script>
-<script language="javascript" type="text/javascript">
-<!--
-function EJEJC_lc(th) { return false; }
-function EJEJC_config(th) {
-    EJEJC_WIDTH = 800;
-    EJEJC_HEIGHT = 600;
-}
-
-// -->
-</script>
-<script src='https://www.e-junkie.com/ecom/box.js' type='text/javascript'></script>
+    <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+  <script src="assets/js/app.js"></script>
 </body>
 </html>

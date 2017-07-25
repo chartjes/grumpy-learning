29c5ad76590e62aead737f6554c61cb0efc9e512
Chris Hartjes <chartjes@littlehart.net>
2017-07-25 16:12:26 -0400

    Updated code to indicate 1 elelphpant left

--- elephpant.php
+++ elephpant.php
@@ -41,13 +41,12 @@
             </p>
             <p><b>(Update July 25: There are only THREE left! Don't miss out on your chance to own a unique piece of PHP memorabilia)</b></p>
             <p>
-          <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-              <input type="hidden" name="cmd" value="_s-xclick">
-              <input type="hidden" name="hosted_button_id" value="6QT8J6NXR7DFE">
-              <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-              <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-          </form>
-
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                <input type="hidden" name="cmd" value="_s-xclick">
+                <input type="hidden" name="hosted_button_id" value="6QT8J6NXR7DFE">
+                <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+            </form>
           </p>
       </h3>
   </div>>

ea43ca9fe0bf4d94580c38a376b0755cb5ca1ae6
Chris Hartjes <chartjes@littlehart.net>
2017-09-14 10:10:23 -0400

    More tweaking of copy and added an entry for Moses Ngone

--- grumpyconf.php
+++ grumpyconf.php
@@ -27,7 +27,7 @@
   <header class="header header--centered">
   <div class="container container--normal">
       <h1 class="header__title">GrumpyConf 2018 </h1>
-      <p class="header__tagline">March 22-24, 2018 -- learn like a grumpy programmer</p>
+      <p class="header__tagline"><b>March 22-24, 2018</b> -- learn like a grumpy programmer</p>
   </div>
   </header>
   <main role-"main">
@@ -38,23 +38,38 @@
     6 high-impact speakers from the PHP world<br>
     1 birthday party<br>
     </p>
+    <p>I helped run <b>True North PHP</b> for five years, and now I wanted to do <b>something different</b>. I
+want to use my <b>conference organizing experience</b> to do something <b>unique</b> for you.</p>
     <p>
-    GrumpyConf 2018 is your chance to hear from some of the PHP community's most impactful speakers in the morning,
-    and then interact with them and your fellow attendees in an <a href="http://www.mindviewinc.com/Conferences/OpenSpaces.html">open spaces</a>
-    environment in the afternoon. Finally, we'll be having a birthday party at the end for Chris (Black Forest cake will be provided!)
+    <b>GrumpyConf 2018</b> is your chance to hear from some of the PHP community's most <b>impactful speakers</b> in the morning,
+    and then <b>interact with them</b> and your fellow attendees in an <b><a href="http://www.mindviewinc.com/Conferences/OpenSpaces.html">open spaces</a></b>
+    environment in the afternoon. Finally, we'll be having a birthday party at the end - it's my 47th birthday! (Black Forest cake will be provided)
     </p> 
     <p>
-    Your ticket is for 3 days of the conference, running March 22-24, 2018. It includes a room at the venue, the beautiful
-    <a href="http://www.elmhurstinn.com/">Elm Hurst Inn</a> in Ingersoll, Ontario (about 90 minutes west of Toronto).
-    Breakfast is provided for guests staying overnight at the Inn, lunch and snacks will be provided to attendees.
+    Your ticket is for <b>3 days</b> of the conference, running March 22-24, 2018. It <b>includes a room</b> at the venue, the beautiful
+    <a href="http://www.elmhurstinn.com/">Elm Hurst Inn</a> in <b>Ingersoll, Ontario, Canada</b> (about 90 minutes west of Toronto).
+    Breakfast is provided for guests staying overnight at the Inn, <b>lunch and snacks</b> will be provided to attendees.
     </p>
     <h2>Speakers</h2>
-    <p>Chris has selected 6 speakers who he feels will push both your PHP skills and your professional skills. So who is coming?</p>
+    <p>I've selected <b>6 speakers</b> who I feel will push both your <b>PHP skills</b> and your <b>professional development</b>. So who is coming?</p>
     <h3>Ed Finkler</h3>
+    <p><img height=240 width=240 src="images/ed_finkler.jpeg" /></p>
     <p>You might know him through his work with <a href="https://osmihelp.org">Open Sourcing Mental Illness</a> or recording
-    a <a href="https://devhell.info">podcast</a> with Chris. He's also a good developer and an even better communicator.
+    a <a href="https://devhell.info">podcast</a> with me. He's also a <b>good developer</b> and an even <b>better communicator</b>.
     </p>
-    <h3>More speakers to be revealed...</h3> 
+    <p>Sure, Ed is a close friend but his message in an <b>important one</b> to help you understand that <b>coding is about people</b>
+    more than it is about the technology</p>
+    <h3>Moses Ngone</h3> 
+    <p><img height=240 width=240 src="images/moses_ngone.jpeg" /></p>
+    <p>There are some folks whom you come across online and you say to yourself
+"I need to figure out how to <b>get this person to speak at my event</b>" and
+things never line up quite right. This time around, I made sure to ask
+Moses to come and talk.</p>
+    <p>His background is one of picking lots of
+<b>interesting (at least to me) projects</b> and his experiences will be
+invaluable to someone looking to <b>build a career</b> full of opportunities to
+work on things that <b>interact with the real world</b>.</p>
+    <h3>Four more speakers to be revealed...</h3>
     <p>Please carefully read the conference's <a href="http://confcodeofconduct.com/">Code of Conduct</a> before purchasing a ticket. 
     Chris strongly believes in a safe and supportive environment for all attendees.</p>
     <tito-widget event="grumpyconf/2018"></tito-widget>

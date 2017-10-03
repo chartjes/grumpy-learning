d714bb4ad85bf20016c6ea260cca95a28dd77d6b
Chris Hartjes <chartjes@littlehart.net>
2017-10-03 12:38:25 -0400

    Updated text for grumpyconf

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
         <div class="package" id="test-driven-basic">
             <h2 align="center">
                 <a href="/grumpyconf.php">
-                    Do you trust me? Blind bird tickets on sale right now<br>for GrumpyConf 2018 starting at CDN$699
+                    Early bird tickets on sale right now<br>for GrumpyConf 2018 starting at CDN$849
                 </a>
             </h2>
         </div>

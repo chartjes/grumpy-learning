baa11f2151d9fcb69fb9d0ea101d6c444cf6654e
Chris Hartjes <chartjes@littlehart.net>
2018-10-29 09:26:11 -0400

    Main GrumpyConf page points to 2019 event

--- grumpyconf.php
+++ grumpyconf.php
@@ -1,79 +1,73 @@
 <!DOCTYPE html>
 <html lang="en">
+<head>
 <title>Learn Like A Grumpy Programmer</title>
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="./assets/css/app.css">
-<script type="text/javascript">
-    WebFontConfig = {
-        google: { families: [ 'Asap:400italic,700italic:latin', 'Source+Sans+Pro:400,300,600,200,300italic,400italic,700:latin' ] }
-    };
-    (function() {
-        var wf = document.createElement('script');
-        wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-            '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-        wf.type = 'text/javascript';
-        wf.async = 'true';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(wf, s);
-    })();
-</script>
-<!-- place this in your head tag -->
+<link rel="stylesheet" type="text/css" href='https://css.tito.io/v1.1' />
+<script src='https://js.tito.io/v1' async></script>
 </head>
   <body>
   <header class="header header--centered">
   <div class="container container--normal">
-      <h1 class="header__title">GrumpyConf 2018 </h1>
-      <p class="header__tagline"><b>March 22-24, 2018</b> -- Learn like a grumpy programmer</p>
+      <h1 class="header__title">GrumpyConf 2019</h1>
+      <p class="header__tagline"><b>March 22-23, 2018</b> -- Learn like a grumpy programmer</p>
   </div>
   </header>
   <main role-"main">
   <div class="container container--normal">
         <h2>High Performance PHP and Professional Development</h2>
-    <p>I helped run <a href="https://truenorthphp.ca"><b>True North PHP</b></a> for five years, and now I wanted to do <b>something different</b>. I
-want to use my <b>conference organizing experience</b> to do something <b>unique</b> for you.</p>
-    <p>
-    <b>GrumpyConf 2018</b> is your chance to hear from some of the <b>PHP community's most impactful speakers</b> in the morning,
-    and then <b>interact with them</b> and your fellow attendees in an <b><a href="http://wintertechforum.com/open-spaces/">open spaces</a></b>
-    environment in the afternoon. This event will be small and focused on <b>developing your technical and personal skills</b>. 
+      <p><b>GrumpyConf is back for 2019!</b></p>
+      <h5>You can see details about last year's event <a href="grumpyconf2018.php">here</a></h5>
+      <p>
+          Back for another edition, GrumpyConf 2019 is your opportunity to listen to some of the <b>PHP community's best speakers</b>
+          before lunch and then learn from them and other attendees via <b><a href="http://wintertechforum.com/open-spaces/">open spaces</a></b>
+          sessions.
     </p>
     <p>
     The venue is the beautiful
-    <a href="http://www.elmhurstinn.com/">Elm Hurst Inn</a> in <b>Ingersoll, Ontario, Canada</b> (about 90 minutes west of Toronto).
-    Breakfast is provided for guests staying overnight at the Inn, <b>lunch and snacks</b> will be provided to attendees. <b>Additional nights at the hotel
-    are available</b> if you need them, just let me know what you require. At this point in time we only have tickets that
-do not include accomodation available.
+    <a href="http://www.elmhurstinn.com/">Elm Hurst Inn</a> in <b>Ingersoll, Ontario, Canada</b> (about 90 minutes west of Toronto.
     </p>
-    <p>Each of the <b>available experts</b> has a specific area of expertise, and you will have the opportunity to <b>discuss your real-world situations</b>
-    in small groups with them. I want you to have the type of <b>access that is difficult to get</b> at traditional conferences. If you've ever heard
+      <p>
+          This year, the <b>All-inclusive ticket</b> includes the following:
+          <ul>
+          <li>conference ticket</li>
+          <li>three nights' stay at the resort (March 21st, 22nd, 23rd)</li>
+          <li>dinner on the 22nd and 23rd</li>
+        </ul>
+      <p>
+      This is in addition to the lunch being provided at the event and breakfast provided by the resort in the mornings.
+      </p>
+      <p>There is a ticket for <b>local attendees</b> that covers attendance and the lunch being provided</p>
+
+    <p>Each of the <b>talented speakers</b> has a specific area of expertise, and you will have the opportunity to <b>discuss your real-world situations</b>
+    in small groups with them. I want you to have the type of <b>access that is almost impossible to get</b> at traditional conferences. If you've ever heard
     of people talk about the "hallway track" at events, well, this entire event is like <b>one big "hallway track"</b></p>
-      <p>Thank you to <a href="https://future500.nl/" target="_blank" rel="noopener noreferrer">Future500</a> for sponsoring this year's event</p>
+      <p>For details on tickets and pricing, please visit our page at <a href="https://ti.to/grumpyconf/grumpyconf-2019">Ti.to</a></p>
   </div>
   <div class="container container--normal">
       <h2>Schedule</h2>
-      <h3>Thursday, March 22</h3>
+      <h3>Thursday, March 21</h3>
       <p>
-          <ul>
-          <li>4:00 PM: Registration and then low-key social activities</li>
-      </ul>
+          Check in at the resort and some light social activities with other attendees.
       </p>
-      <h3>Friday, March 23</h3>
+      <h3>Friday, March 22</h3>
       <p>
           <ul>
-            <li>09:30 AM: Chris Hartjes -- <b>Event kick-off</b></li>
-            <li>10:00 AM: Elizabeth Marie Smith -- <b>PHP architecture and SAPIs</b></li>
-            <li>11:00 AM: Moses Ngone -- <b>Developing Beyond The Web</li>
+            <li>09:00 AM: Registration</li>
+            <li>09:45 AM: Chris Hartjes -- <b>Event kick-off</b></li>
+            <li>10:00 AM: Elizabeth Marie Smith</li>
+            <li>11:00 AM: Mark Story</li>
             <li>12:00 PM: Lunch and then open spaces until 5pm!</li>
           </ul>
       </p>
-      <h3>Saturday, March 24</h3>
+      <h3>Saturday, March 23</h3>
       <p>
         <ul>
-          <li>09:00 AM: Lorna Mitchell -- <b>Roadtrip Through Database Country</b></li>
-          <li>10:00 AM: Matthew Weier-O'Phinney -- <b>The Promise of Middleware</b></li>
-          <li>11:00 AM: Sara Golemon -- <b>High Performance Developer Efficiency</b></li>
-          <li>12:00 PM: Lunch and then open spaces!</li>
-          <li>05:00 PM: Birthday party for Chris!</li>
+          <li>10:00 AM: Gemma Anible</li>
+          <li>11:00 AM: Marco Pivetta</li>
+          <li>12:00 PM: Lunch and then open spaces until 5pm!</li>
         </ul>
       </p>
   </div>
@@ -84,69 +78,40 @@ do not include accomodation available.
   </div>
   <div class="container container--normal">
     <h2>Speakers</h2>
-    <p>I've selected <b>5 speakers</b> who I feel will push both your <b>PHP skills</b> and your <b>professional development</b>. So who is speaking?</p>
-    <h3>Moses Ngone</h3> 
-    <p><img height=240 width=240 src="images/moses_ngone.jpeg" /></p>
-    <p>There are some folks whom you come across online and you say to yourself
-"I need to figure out how to <b>get this person to speak at my event</b>" and
-things never line up quite right. This time around, I made sure to ask
-Moses to come and talk.</p>
-    <p>His background is one of picking lots of
-<b>interesting (at least to me) projects</b> and his experiences will be
-invaluable to someone looking to <b>build a career</b> full of opportunities to
-work on things that <b>interact with the real world</b>.</p>
-      <h4>Developing Beyond The Web</h4>
-      <p>The fast development of "smart" devices and their demand has opened up new avenues for the web developer. This session will look at the new opportunities, challenges and how one can leverage skills, tools and processes already available to the modern day web developer. </p>
-    <h3>Sara Golemon</h3>
-    <p><img height=240 width=240 src="images/sara_golemon.jpeg" /></p>
-    <p>She's part of the <b>core PHP team</b>, the <b>release manager</b> for PHP 7.2 and worked on <b>HHVM and Hack</b>.  She also
-    always calls me Grumpy. Sara is an <b>entertaining speaker</b> who loves talking about <b>programming languages</b> She spoke
-    at TrueNorthPHP and I'm happy she accepted my invitation to come and speak at GrumpyConf 2018.
-    </p>
-    <h4>High Performance Developer Efficiency</h4>
-    <p>The performance war between PHP and HHVM is over, but
-HackLang still leads the way in language innovation and developer
-efficiency.  In this presentation, Sara will introduce you to the the
-PHP that might have been and still can be.</p>
-    <h3>Elizabeth Marie Smith</h3>
-        <p><img height=240 width=240 src="images/elizabethsmith.jpg" /></p>
+    <p>I've selected <b>4 speakers</b> who I feel have a combination of <b>technical and personal skills</b> that can  So who is speaking?</p>
+      <h3>Elizabeth Marie Smith</h3>
+      <p><img height=240 width=240 src="images/elizabethsmith.jpg" /></p>
       <p>Elizabeth is the person who <b>got me into mentoring</b> with her infectious energy surrounding <b>teaching people</b></p>
       <p>She is one of the few people putting a lot of work into making <b>PHP run on Windows</b> and has probably forgotten more
-      about the internals of PHP than I will ever know. I always come away from her talks saying to myself <b>"wow, I knew way less about this than I thought"</b></p>
-
-    <h3>Matthew Weier-O'Phinney</h3>
-        <p><img height=240 width=240 src="images/mwop.jpeg" /></p>
-        <p>From <b>divinity major at college</b> to "Supreme Allied Commander" of <b>Zend Framework</b>, Matthew is one
-        of the most <b>well-rounded</b> people I've gotten to know over my years in this industry. I had been trying for years
-        to get him to speak at my events, and I'm <b>grateful he accepted</b> my invitation this time.
-        </p>
-        <p>Matthew is also one of the lucky few that was inspired by my <b>4-day work week</b> strategy and made it work for himself!</p> 
-        <h4>The Promise of Middleware</h4>
+          about the internals of PHP than I will ever know. I always come away from her talks saying to myself <b>"wow, I knew way less about this than I thought"</b></p>
+    <h3>Mark Story</h3>
+    <p><img height=240 width=240 src="images/mark-story-2018.jpg" /></p>
+      <p>
+          I've known Mark for quite a while through my experiences with the <b>CakePHP framework</b>, where Mark is the lead developer.
+          Mark is a "local" (living in the Greater Toronto Area) and is undefeated against me in games of Magic The Gathering.
+      </p>
+      <h4>So you want to run an open source project?</h4>
+      <p>9 years ago, I thought it would be a good idea to get involved in Open Source being
+          an Art Major who had no experience writing software professionally. In the intervening
+          years I've been fortunate enough to build a global community and work with folks from
+          every continent.
+      </p>
         <p>
-            Several years ago, a movement towards PHP microframeworks began, and largely
-            fell by the wayside. The main problem was interoperability: every framework
-            provided its own abstractions around HTTP messages, and the contracts around
-            dispatching code varied greatly.
-        </p>
-        <p>
-            In recent years, however, PHP has undergone a renaissance, in large part due to
-            the rise of standards and the ease of managing dependencies via Composer.
-            Middleware finally fulfills the promise of MicroPHP, in ways many never
-            envisioned.
-        </p>
-        <p>
-            Find out how middleware can simplify your development of applications, both
-            small and complex, and how else it can benefit your development and teams.
-        </p>
-    <h3>Lorna Mitchell</h3>
-        <p><img height=240 width=240 src="images/lornajane.jpeg" /></p>
-        <p>The very first time I <b>saw Lorna speak</b> she did it with <b>no slides</b>, which blew my mind! She is also another speaker whom I have invited over the years to hop onto a plane and fly across the Atlantic to come to Canada and speak.</p>
-        <p>Lorna always <b>packs a lot of knowledge</b> into her talks, and I never come away from any of her talks without <b>learning something new</b>. Lorna's talent lies in <b>cutting through complexity</b> and teaching you <b>what you really need to know</b>.</p>
-    <h4>Roadtrip Through Database Country</h4>
-    <p>Databases must be reliable.  As a result, developers mostly stick with the tools they already know and trust, leaning more on the side of safety than innovation when choosing a database.  This is totally reasonable, but if you're curious then let's put the top down and take a road trip around Database Country, looking at the options that are out there and what might lead us to choose them as our destination one day.  We'll start with the well-trodden streets of relational databases, the head out to the rolling hills of NoSQL to check out document databases, graph databases, and more.  This high-level tour of the landscape will give you some pointers for what's available, which problems they solve, when to combine solutions - and when to simply turn tail and head for home. 
-</p>
+          In this talk I'd like to share my experiences running a sizable open source project
+          and some of the activities, behaviors and organizational tactics could help
+          you on your own open source journey.
+      </p>
+    <h3>Gemma Anible</h3>
+        <p><img height=240 width=240 src="images/gemma-anible.jpg" /></p>
+      <p>Gemma works from the <b>fjords of Norway</b> and is going to <b>share her experiences</b> with something she did TWICE that makes a lot of developers lose sleep: <b>migrating
+          your code to newer versions of a PHP framework</b></p>
+    <h3>Marco Pivetta</h3>
+        <p><img height=240 width=240 src="images/marco-pivetta.jpg" /></p>
+      <p>I worked with Marco as part of a <b>developer co-op</b> and learned just how skilled and talented he really is. He is a
+      <b>contributor to Doctrine and Zend Framework</b> and cares about <b>code quality, maintainability, and testing<b></b> in a way that few other
+      people I have come across exhibit.</p>
     <h2>Conference Code Of Conduct</h2>
-    <p>Please carefully read the conference's <a href="http://confcodeofconduct.com/">Code of Conduct</a> before purchasing a ticket. 
+    <p>Please carefully read the conference's <a href="http://confcodeofconduct.com/">Code of Conduct</a> before purchasing a ticket.
     I strongly believe in a safe and supportive environment for all attendees.</p>
   </div>
   </main>

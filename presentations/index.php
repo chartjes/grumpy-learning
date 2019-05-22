5dd848f8d01e8ebd58f66202206f16d43545595f
Chris Hartjes <chartjes@littlehart.net>
2019-05-22 14:41:20 -0400

    Added in new presentations section

---
+++ presentations/index.php
@@ -0,0 +1,9 @@
+<h1>Presentations</h1>
+<p>
+This is an incomplete list of presentations
+I have given, but more will appear as I extract
+them from some existing presentation hosting
+sites.
+</p>
+
+<a href="./tek2019">May 2019 - phptek 2019</a>
---
+++ presentations/tek2019/index.php
@@ -0,0 +1,21 @@
+<h1>phptek 2019</h1>
+
+<h3>May 23, 2019</h3>
+<p>
+At this event I did the testing track for the event, 
+consisting of 7 sesssions. The third session slides
+are not included because the session was me live coding
+a test example for the attendees to use and extend.
+</p>
+<p>
+These slides are free for you to download and use.
+</p>
+<p>
+<ul>
+<li><a href="testing-workshop-01.pdf">Five Factors of Testing</a></li>
+<li><a href="testing-workshop-02.pdf">What Do Tests Look Like?</a></li>
+<li><a href="testing-workshop-04.pdf">Test Doubles</a></li>
+<li><a href="testing-workshop-05.pdf">Test-after Techniques</a></li>
+<li><a href="testing-workshop-06.pdf">Test-adjacent Tools</a></li>
+<li><a href="testing-workshop-07.pdf">Tools & Technique Comparisons</a></li>
+</p>
